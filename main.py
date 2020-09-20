85ee3beaba09940dbb5bed369a32bc926c04feb1
constantin-kuehne <70445955+constantin-kuehne@users.noreply.github.com>
2020-09-20 23:58:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,31 +25,15 @@ def search_result(search_input):
 def main():
 	from test_request import test
 	search_input = ""
+	
 	if request.method == "POST":
 		search_input = request.form["search_input"]
 		search_input = search_input.lower().strip()
 		
 	if search_input == "":
-		return render_template('home.html')
-		
-		"""
-		name = 'instagram'
-		site = insta_site('instagram')
-		if site.status_code:
-			img_path = site.get_img()
-			followers, following, posts, description = site.get_info()
-			return render_template('home.html', name=name, followers=followers, following=following, posts=posts, img_path=img_path)"""
-			
+		return render_template('home.html')		
 	else:
 		return redirect(url_for(".search_result", search_input=search_input))
-		
-	"""else:
-		name = search_input
-		site = insta_site(search_input)
-		if site.status_code:
-			img_path = '/' + site.get_img()
-			followers, following, posts, description = site.get_info()
-			return render_template('search_result.html', name=name, followers=followers, following=following, posts=posts, img_path=img_path)"""
 	
 
 # Running on http://127.0.0.1:5000/
